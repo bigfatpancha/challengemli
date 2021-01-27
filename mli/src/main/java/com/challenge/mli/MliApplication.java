b8b42282b2ade6aecffd5ea2316907c4e7f0b137
Lucía Juliá Rodriguez <ljulia@certant.com>
2021-01-26 23:15:27 -0300

    init

---
+++ mli/src/main/java/com/challenge/mli/MliApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.mli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MliApplication.class, args);
+	}
+
+}
---
+++ mli/src/test/java/com/challenge/mli/MliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.mli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
